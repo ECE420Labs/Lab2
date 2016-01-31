599caba1afa1d94ef2d75ecdfededacc72453440
Andrea McIntosh <akmcinto@ualberta.ca>
2016-01-30 21:31:10 -0700

    Added comments

--- lab2sol_sem.c
+++ lab2sol_sem.c
@@ -23,35 +23,38 @@ int main (int argc, char* argv[])
     long thread_i;
     pthread_t* thread_handle_pt;
     double start, end;
-    
+
     if (argc < 2) {
         printf("Please indicate the number of threads!\n");
         return 1;
     }
     thread_count_ = strtol(argv[1], NULL, 10);
-    
+
     Lab2_loadinput(&dp_, &city_count_);
 
-    thread_handle_pt=malloc(thread_count_*sizeof(pthread_t));
-
+    // Allocate all threads
+    thread_handle_pt = malloc(thread_count_*sizeof(pthread_t));
+    // Allocate all mutexes; need one for each row (city_count_)
     muts = malloc(city_count_ * sizeof(pthread_mutex_t));
 
     int z;
+    // Initialize mutexes
     for (z = 0; z < city_count_; z++) {
-      pthread_mutex_init(&muts[z], NULL);
+        pthread_mutex_init(&muts[z], NULL);
     }
-
     GET_TIME(start);
-    for (thread_i = 0; thread_i < thread_count_; ++thread_i)
+    for (thread_i = 0; thread_i < thread_count_; ++thread_i) {
         pthread_create(&thread_handle_pt[thread_i], NULL, thread_subcal, (void*)thread_i);
-    for (thread_i = 0; thread_i < thread_count_; ++thread_i)
+    }
+    for (thread_i = 0; thread_i < thread_count_; ++thread_i) {
         pthread_join(thread_handle_pt[thread_i], NULL);
+    }
     GET_TIME(end);
 
     printf("The elapsed time is %lfs.\n", end-start);
 
     for (z = 0; z < city_count_; z++) {
-      pthread_mutex_destroy(&muts[z]);
+        pthread_mutex_destroy(&muts[z]);
     }
     free(thread_handle_pt);
     Lab2_saveoutput(dp_, city_count_, end-start);
@@ -63,23 +66,31 @@ int main (int argc, char* argv[])
 void* thread_subcal(void* rank){
     long myrank = (long)rank;
     int i, j, k, temp;
-    
+
+    /*
+    On each iteration a thread will access elements along rows k and i.
+    Both of these rows need to be protected by mutexes when a thread is
+    accessing them (reading from rows i and k, and writing to row i).
+    */
     for (k = 0; k < city_count_; ++k) {
-      pthread_mutex_lock(&muts[k]);
-      for (i = myrank * city_count_ / thread_count_; i < (myrank + 1) * city_count_ / thread_count_; ++i) {
-	if (i != k) {
-	  pthread_mutex_lock(&muts[i]);
-	}
-	for (j = 0; j < city_count_; ++j) {
+        pthread_mutex_lock(&muts[k]);
+        // Each thread is responsible for rows from (myrank * city_count_ / thread_count_)
+        // to ((myrank + 1) * city_count_ / thread_count)
+        for (i = myrank * city_count_ / thread_count_; i < (myrank + 1) * city_count_ / thread_count_; ++i) {
+            if (i != k) {
+                // If i and k are equal, do not have to lock row k again
+                pthread_mutex_lock(&muts[i]);
+            }
+            for (j = 0; j < city_count_; ++j) {
                 if ((temp = dp_[i][k]+dp_[k][j]) < dp_[i][j])
-                    dp_[i][j] = temp; 
-	}
-	if (i != k) {
-	  pthread_mutex_unlock(&muts[i]);
-	}
-      }
-      pthread_mutex_unlock(&muts[k]);
+                dp_[i][j] = temp;
+            }
+            if (i != k) {
+                // If i and k are equal, do not free row k yet
+                pthread_mutex_unlock(&muts[i]);
+            }
+        }
+        pthread_mutex_unlock(&muts[k]);
     }
     return 0;
 }
-
